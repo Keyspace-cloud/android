0dca53f40f1d597313944c912e90690839fbb2d9
Owais Shaikh <owais.shaikh@redhuntlabs.com>
2022-12-20 10:00:53 +0530

    -

--- app/src/main/kotlin/cloud/keyspace/android/StartHere.kt
+++ app/src/main/kotlin/cloud/keyspace/android/StartHere.kt
@@ -74,6 +74,7 @@ class StartHere : AppCompatActivity() {
     private lateinit var misc: MiscUtilities
     private lateinit var configData: SharedPreferences
 
+
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var keyring: CryptoUtilities.Keyring
 
